c9b1d2f26a33699b240d29aac07ffbbc22165a3e
Tejas Jain <jaintj95@users.noreply.github.com>
2018-08-07 23:13:02 +0530

    Upload user data to Firebase Database

--- app/src/main/java/com/tj/kotlinmessenger/MainActivity.kt
+++ app/src/main/java/com/tj/kotlinmessenger/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.tj.kotlinmessenger
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
@@ -17,11 +24,40 @@ class MainActivity : AppCompatActivity() {
 
         button_register.setOnClickListener {
             performRegister()
+
         }
 
         textView_account_already.setOnClickListener {
             val intent = Intent(this,LoginActivity::class.java)
             startActivity(intent)
+        }
+
+        button_select_photo.setOnClickListener{
+            Log.d("MainActivity","Try to show photo selector")
+
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent,0)
+
+        }
+    }
+
+    var selectedPhotoUri: Uri? = null
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == 0 && resultCode == Activity.RESULT_OK && data != null)
+        {
+            Log.d("MainActivity","Photo was selected")
+            selectedPhotoUri = data.data
+
+            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver,selectedPhotoUri)
+
+            selectphoto_imageview.setImageBitmap(bitmap)
+            button_select_photo.alpha = 0f
+//            val bitmapDrawable = BitmapDrawable(bitmap)
+//            button_select_photo.setBackgroundDrawable(bitmapDrawable)
+
         }
     }
 
@@ -42,10 +78,40 @@ class MainActivity : AppCompatActivity() {
                     if(!it.isSuccessful) return@addOnCompleteListener
 
                     Log.d("Main","Successfully created user with UID: ${it.result.user.uid}")
+                    uploadImageToFirebaseStorage()
                 }
                 .addOnFailureListener{
                     Log.d("Main","Failed to create user: ${it.message}")
                     Toast.makeText(this,"Email format is incorrect",Toast.LENGTH_SHORT).show()
                 }
     }
+
+    private fun uploadImageToFirebaseStorage(){
+        if(selectedPhotoUri == null) return
+        val filename = UUID.randomUUID().toString()
+        val ref = FirebaseStorage.getInstance().getReference("/images/$filename")
+        ref.putFile(selectedPhotoUri!!)
+                .addOnSuccessListener {
+                    Log.d("Register","Successfully uploaded image: ${it.metadata?.path}")
+
+                    ref.downloadUrl.addOnSuccessListener {
+                        Log.d("RegisterActivity","File location: $it")
+
+                        saveUserToFirebaseDatabase(it.toString())
+                    }
+                }
+    }
+
+    private fun saveUserToFirebaseDatabase(profileImageUrl: String)
+    {
+        val uid = FirebaseAuth.getInstance().uid ?: ""
+        val ref = FirebaseDatabase.getInstance().getReference("/users/$uid")
+        val user = User(uid, editText_username.text.toString(), profileImageUrl)
+        ref.setValue(user)
+                .addOnSuccessListener {
+                    Log.d("RegisterActivity","Finally we saved the user to Firebase Database.")
+                }
+    }
 }
+
+class User(val uid:String, val username: String, val profileImageUrl:String)
